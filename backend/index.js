2b097960f3cbf5f67aec4174bf73f07a24a0471c
Jack Ragless <jackdragless@gmail.com>
2023-01-24 12:34:21 +1030

    removed .env usage (test)

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ const mc = mongodb.MongoClient
 import dotenv from "dotenv"
 dotenv.config()
 
-const db_uri = process.env.DB_URI
+// const db_uri = process.env.DB_URI
+const db_uri =
+  "mongodb+srv://jackragless:mernstack@cluster0.she2tlp.mongodb.net/?retryWrites=true&w=majority"
 
-const port = process.env.PORT
+// const port = process.env.PORT
+const port = 3000
 
 mc.connect(db_uri).then(async (client) => {
   await dbapi.inject(client)
