a068a3753efdab2e1eeab7f3ec336200d98cf4ce
jragless <jackdragless@gmail.com>
2023-10-06 00:13:34 +1100

    fixed vercel app backend url

--- frontend-ts/src/services/Resume.service.tsx
+++ frontend-ts/src/services/Resume.service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const getResume = async () => {
-  const res = await axios.get(`https://programmer-portfolio-app.vercel/api/resume`)
+  const res = await axios.get(`https://programmer-portfolio-app.vercel.app/api/resume`)
   return res.data[0]
 }
 export default getResume
